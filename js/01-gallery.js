81076e67c4bd0fcfa4c7d5d954f41b49aa25bcf8
ApolinarySzumski <apolinary@poczta.onet.pl>
2023-04-01 20:15:07 +0200

    finished 1st task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const ulGallery = document.querySelector(".gallery");
+
+const addToGallery = galleryItems
+  .map(
+    (galleryItem) => `<div class="gallery__item">
+  <a class="gallery__link" href=${galleryItem.original}>
+    <img
+      class="gallery__image"
+      src=${galleryItem.preview}
+      data-source=${galleryItem.original}
+      alt=${galleryItem.description}
+    />
+  </a>
+</div>`,
+  )
+  .join("");
+
+ulGallery.insertAdjacentHTML("afterbegin", addToGallery);
+
+const selectPhoto = (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="1280" height="855">
+  `);
+  instance.show();
+
+  ulGallery.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+};
+
+ulGallery.addEventListener("click", selectPhoto);
